812d96b9dbce7c5bc1e189f1eddda500f9990a47
pkid1729 <pkid1729@gmail.com>
2018-09-08 01:35:43 -0700

    Initial Commit

---
+++ TestFigure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AbstracImplimentation
+{
+     class TestFigures
+    {
+      static void Main()
+        {
+            //congo..
+            //ha hahahahahhahaha
+             Rectangle r=new Rectangle(87.17,97.85);
+             Circle c =new Circle(47.91);
+           // Rectangle r=new Rectangle(87,97);
+          //  Circle c =new Circle(2);
+            Console.WriteLine("Area of rectangle"+r.GetArea());
+            Console.WriteLine("Area of circle"+c.GetArea());
+            Console.ReadLine();
+            
+        }
+    }
+    public abstract class Figure
+    {
+        // static void Main(string[] args)
+        // {
+           
+        // }
+        public double width,height,radious;
+        public const float pi=3.14f;
+        public abstract double GetArea();
+
+        
+    }
+    public class Rectangle:Figure
+    {
+        public Rectangle(double width,double height)
+        {
+            this.width=width;
+            this.height=height;
+        }
+        public override double GetArea()
+        {
+            return width*height;
+        }
+    }
+    public class Circle:Figure
+    {
+        public Circle(double radious)
+        {
+            this.radious=radious;
+        }
+        public override double GetArea()
+        {
+            return pi*radious*radious;
+        }
+
+    }
+   
+}
